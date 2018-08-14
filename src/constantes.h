c6332d37355827c118ecb8d949fa66c87224337b
axel POULAT <35915186+sganayon@users.noreply.github.com>
2018-08-14 15:22:07 +0200

    Update constantes.h

--- src/constantes.h
+++ src/constantes.h
@@ -3,8 +3,11 @@
 
 //Toutes les constantes sont ici
 
-//Taille max de la fenetre de rendu
-static const unsigned int WINDOW_SIZE = 800;
+//Taille max de la fenetre de rendu en x
+static const unsigned int WINDOW_SIZE_X = 800;
+
+//Taille max de la fenetre de rendu en y
+static const unsigned int WINDOW_SIZE_Y = 600;
 
 //Enum des bonus
 static const enum Bonus
