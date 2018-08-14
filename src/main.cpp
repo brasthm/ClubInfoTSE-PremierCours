5203b988de36c820a0920235a8827b854a2ca21f
axel POULAT <35915186+sganayon@users.noreply.github.com>
2018-08-14 15:25:33 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "constantes.h"
 
 int main()
 {
-	//Création de la fenetre du jeux
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SUPER RUNNER");
+	//CrÃ©ation de la fenetre du jeux
+	sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "SUPER RUNNER");
 	
 	//Tant que l'on joue (fenetre ouverte)
 	while (window.isOpen())
 	{
-		//Création d'un objet récupérant les événements (touche clavier et autre)
+		//CrÃ©ation d'un objet rÃ©cupÃ©rant les Ã©vÃ©nements (touche clavier et autre)
 		sf::Event event;
 
-		//Boucle des évennements
+		//Boucle des Ã©vennements
 		while (window.pollEvent(event))
 		{	
 			//Evenement de fermeture de la fenetre : on ferme le jeux
