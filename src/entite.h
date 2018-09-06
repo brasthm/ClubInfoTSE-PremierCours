3002e01ddbfea91438ef03a6aadfc95324da81af
Axel POULAT <axel.poulat1@gmail.com>
2018-09-06 16:40:16 +0200

    fuck

--- src/entite.h
+++ src/entite.h
@@ -23,9 +23,9 @@ public :
 	void setContour(sf::RectangleShape shape) { contour_ = shape.getGlobalBounds(); }
 	void setContour(sf::Sprite sprite) { contour_ = sprite.getGlobalBounds(); }
 	void setContour(AnimatedSprite& animatedSprite) { contour_ = animatedSprite.getGlobalBounds(); }
-	void draw(sf::RenderWindow& window, sf::RectangleShape shape) { window.draw(shape); }
-	void draw(sf::RenderWindow& window, sf::Sprite sprite) { window.draw(sprite); }
-	void draw(sf::RenderWindow& window, AnimatedSprite& animatedSprite, const sf::Time& elapsedTime) { animatedSprite.Animer(elapsedTime); animatedSprite.draw(window); }
+	void draw(sf::RenderWindow& window, sf::RectangleShape shape) { shape.setPosition(0,positionY_); window.draw(shape); }
+	void draw(sf::RenderWindow& window, sf::Sprite sprite) {sprite.setPosition(0, positionY_); window.draw(sprite); }
+	void draw(sf::RenderWindow& window, AnimatedSprite& animatedSprite, const sf::Time& elapsedTime) { animatedSprite.setPosition({ 0,positionY_ }); animatedSprite.Animer(elapsedTime); animatedSprite.draw(window); }
 	bool isCollision(const Obstacle& obstacle);
 	void jump();
 };
@@ -36,15 +36,15 @@ public :
 	sf::Vector2f position_;
 	sf::FloatRect contour_;
 private : 
-	Obstacle() {}
+	Obstacle(float x, float y) { position_ = { x,y }; }
 	void setContour(sf::RectangleShape shape) { contour_ = shape.getGlobalBounds(); }
 	void setContour(sf::Sprite sprite) { contour_ = sprite.getGlobalBounds(); }
 	void setContour(AnimatedSprite& animatedSprite) { contour_ = animatedSprite.getGlobalBounds(); }
-	void draw(sf::RenderWindow& window, sf::RectangleShape shape) { window.draw(shape); }
-	void draw(sf::RenderWindow& window, sf::Sprite sprite) { window.draw(sprite); }
-	void draw(sf::RenderWindow& window, AnimatedSprite& animatedSprite, const sf::Time& elapsedTime) { animatedSprite.Animer(elapsedTime); animatedSprite.draw(window); }
+	void draw(sf::RenderWindow& window, sf::RectangleShape shape) { shape.setPosition(position_); window.draw(shape); }
+	void draw(sf::RenderWindow& window, sf::Sprite sprite) { sprite.setPosition(position_); window.draw(sprite); }
+	void draw(sf::RenderWindow& window, AnimatedSprite& animatedSprite, const sf::Time& elapsedTime) { animatedSprite.setPosition(position_); animatedSprite.Animer(elapsedTime); animatedSprite.draw(window); }
 	void move(float x, float y) { position_.x += x; position_.y += y; }
-	const sf::FloatRect& getContour() { return contour_; }
+	const sf::FloatRect& getContour() const { return contour_; }
 };
 #endif // !ENTITE_H
 
